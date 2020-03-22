a04c6267612a1571ec79f04c1925bb5214608a4b
Kaelin Laundry <wasabifan@outlook.com>
2020-03-22 03:32:17 -0700

    Framework for visualizer UI

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,75 @@ mod resample;
 mod geometry;
 mod localization;
 
-use resample::resample;
+use geometry::Point;
+use localization::{LocalizationParticleFilter, Map, OdometryMeasurement};
 
-fn main() {
-    let values: [f64; 5] = [1.0, 2.0, 3.0, 4.0, 5.0];
-    let weights: [f64; 5] = [0.1, 0.2, 0.4, 0.2, 0.1];
-    let mut hist = [0usize; 5];
+use std::error::Error;
 
-    for _ in 0..10000 {
-        let result = resample(&values, &weights);
-        for v in result {
-            hist[(v - 1.) as usize] += 1;
+use minifb::{Key, KeyRepeat, Window, WindowOptions};
+use plotters::drawing::bitmap_pixel::BGRXPixel;
+use plotters::element::*;
+use plotters::prelude::*;
+
+const MAP_WIDTH: usize = 200;
+const MAP_HEIGHT: usize = 200;
+
+const WIDTH: usize = MAP_WIDTH * 2;
+const HEIGHT: usize = MAP_HEIGHT * 2;
+
+fn map_point_to_window(point: Point) -> (i32, i32) {
+    ((point.x / MAP_WIDTH as f64 * WIDTH as f64) as i32, (point.y / MAP_HEIGHT as f64 * HEIGHT as f64) as i32)
+}
+
+enum MCLPhase {
+    Predict, Update
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut buf = vec![0u8; WIDTH * HEIGHT * 4];
+    let mut window = Window::new(
+        "Monte Carlo Localization Demo",
+        WIDTH,
+        HEIGHT,
+        WindowOptions::default(),
+    )?;
+
+    let mut mcl = LocalizationParticleFilter::new(100, Map { width: MAP_WIDTH, height: MAP_HEIGHT });
+    let mut next_phase: MCLPhase = MCLPhase::Predict;
+
+    while window.is_open() && !window.is_key_down(Key::Escape) {
+
+        if let Some(keys) = window.get_keys_pressed(KeyRepeat::No) {
+            for key in keys {
+                match key {
+                    Key::Space => {
+                        next_phase = match next_phase {
+                            MCLPhase::Predict => {
+                                mcl.predict(OdometryMeasurement { vx: 10.0, vy: 0.0, sigma: 3.0 });
+                                MCLPhase::Update
+                            }
+                            MCLPhase::Update => MCLPhase::Predict
+                        }
+                    }
+                    _ => {
+                        continue;
+                    }
+                }
+            }
         }
+
+        let root =
+        BitMapBackend::<BGRXPixel>::with_buffer_and_format(&mut buf[..], (WIDTH as u32, HEIGHT as u32))?
+            .into_drawing_area();
+        root.fill(&BLACK)?;
+
+        for particle in &mcl.particles {
+            let (x, y) = map_point_to_window(particle.location);
+            root.draw(&Circle::new((x, y), 1, ShapeStyle::from(&RED).filled()))?;
+        }
+        drop(root);
+        window.update_with_buffer(unsafe { std::mem::transmute(&buf[..]) }, WIDTH, HEIGHT)?;
     }
 
-    println!("{:?}", hist);
+    Ok(())
 }
